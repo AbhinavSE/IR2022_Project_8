65e37c92cf7952c50c8a1354e4f4840e5dd12b77
Shaunak Pal <palshaunak7@gmail.com>
2022-04-19 21:35:11 +0530

    Update

--- App/layout/navbar.py
+++ App/layout/navbar.py
@@ -1,62 +1,24 @@
-from app import app
 import dash_bootstrap_components as dbc
 from utils.constants import PATHS
-from dash import html
-from dash.dependencies import Input, Output, State
-PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
-
-search_bar = dbc.Row(
-    [
-        dbc.Col(dbc.Input(type="search", placeholder="Search")),
-        dbc.Col(
-            dbc.Button(
-                "Search", color="primary", className="ms-2", n_clicks=0
-            ),
-            width="auto",
+navbar = dbc.NavbarSimple(
+    className='sticky-top',
+    children=[
+        dbc.NavItem(dbc.NavLink("Home", href=PATHS['index'])),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("News", href='#'),
+                dbc.DropdownMenuItem("Tags", href='#'),
+                dbc.DropdownMenuItem("Trends", href='#'),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Aggregate",
         ),
     ],
-    className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
-    align="center",
-)
-
-navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            html.A(
-                # Use row and col to control vertical alignment of logo / brand
-                dbc.Row(
-                    [
-                        dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
-                        dbc.Col(dbc.NavbarBrand("RECOM", className="ms-2")),
-                    ],
-                    align="left",
-                    className="g-0",
-                ),
-                href="https://plotly.com",
-                style={"textDecoration": "none"},
-            ),
-            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
-            dbc.Collapse(
-                search_bar,
-                id="navbar-collapse",
-                is_open=False,
-                navbar=True,
-            ),
-        ]
-    ),
-    color="dark",
+    brand="Music Recommender",
+    brand_href="#",
+    color="primary",
     dark=True,
+    links_left=True,
 )
-
-
-# add callback for toggling the collapse on small screens
-@app.callback(
-    Output("navbar-collapse", "is_open"),
-    [Input("navbar-toggler", "n_clicks")],
-    [State("navbar-collapse", "is_open")],
-)
-def toggle_navbar_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
