5394bda463d8919f51adfc57a511b6bafe8c5f2f
Shaunak Pal <palshaunak7@gmail.com>
2022-04-21 03:39:48 +0530

    Update

--- App/pages/music/music_helper.py
+++ App/pages/music/music_helper.py
@@ -27,7 +27,7 @@ def music_cards():
     for i in range(0, len(music), 5):
         cards.append(
             dbc.Row(
-                [dbc.Col([get_card(music[i]['song'], music[i]['artist'], music[i]['img'], '#')]) for i in range(i, min(len(music), i + 5))]
+                [dbc.Col([get_card(music[i]['Title'], music[i]['Artist'], music[i]['image_folder'], '#')]) for i in range(i, min(len(music), i + 5))]
             )
         )
         cards.append(html.Br())
--- App/utils/data.py
+++ App/utils/data.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 
 def load_music():
-    # music_df = pd.read_csv('assets/data/music.csv')
-    music = [
-        {'artist': 'Gym Class Heroes', 'song': 'Stereo Hearts', 'album': 'Stereo Hearts', 'img': 'assets/album_covers/stereo_hearts.jpeg'},
-        {'artist': 'Alan Walker', 'song': 'Faded', 'album': 'Faded', 'img': 'assets/album_covers/faded.jpg'},
-    ]
+    music_df = pd.read_csv('data/metadata.csv')
+    music_df['image_folder'] = music_df['image_folder'].apply(lambda x: 'data' + x if type(x) == str else x)
+    # music = [
+    #     {'artist': 'Gym Class Heroes', 'song': 'Stereo Hearts', 'album': 'Stereo Hearts', 'img': 'assets/album_covers/stereo_hearts.jpeg'},
+    #     {'artist': 'Alan Walker', 'song': 'Faded', 'album': 'Faded', 'img': 'assets/album_covers/faded.jpg'},
+    # ]
     # Repeat the above list to create a list of dictionaries
-    music = music * 5
-    return music
+    return music_df.to_dict('records')[:20]
