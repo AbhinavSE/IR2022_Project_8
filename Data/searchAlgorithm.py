8e4f38df92bb8a1893bdac79d283155d3bdab6a7
Shaunak Pal <palshaunak7@gmail.com>
2022-04-27 19:10:49 +0530

    Update

--- Data/searchAlgorithm.py
+++ Data/searchAlgorithm.py
@@ -1,4 +1,3 @@
-from pytrie import StringTrie
 import pandas as pd
 import time
 import re
@@ -7,146 +6,140 @@ from pprint import pprint
 import json
 import lyricsgenius as lg
 
+
 class Search:
 
-	def __init__(self):
-		self.grams = {
-			'uni': defaultdict(list),
-			'bi': defaultdict(list),
-			'tri': defaultdict(list)
-		}
-		self.map = defaultdict(list)
-		self.initGrams()
-		self.genius = lg.Genius('M18NYItwSvk9kctzrPF1rOlO7YAvFHUpSrepez3VT0FE81uKb8s5-BBkb-HhGHgg', skip_non_songs = True, remove_section_headers = True, excluded_terms = ['(Remix)', '(Live)'])
-		
+    def __init__(self):
+        self.grams = {
+            'uni': defaultdict(list),
+            'bi': defaultdict(list),
+            'tri': defaultdict(list)
+        }
+        self.map = defaultdict(list)
+        self.initGrams()
+        self.genius = lg.Genius('M18NYItwSvk9kctzrPF1rOlO7YAvFHUpSrepez3VT0FE81uKb8s5-BBkb-HhGHgg', skip_non_songs=True, remove_section_headers=True, excluded_terms=['(Remix)', '(Live)'])
 
-	def addGrams(self, wordSet):
-		for key in wordSet:
-			for word in key.split():
-				if word:
-					self.grams['uni'][word[0]].append(key)
-					if len(word) > 1:
-						self.grams['bi'][word[:2]].append(key)
-					if len(word) > 2:
-						self.grams['tri'][word[:3]].append(key)
-	
-	def getValues(self, value, splitWord = False):
-		valueSet = set()
-		
-		for index, v in enumerate(value):
-			v = v.lower()
-			key = re.sub(r'[^\w\s]', ' ', v)
-			if splitWord:
-				for word in key.split():
-					self.map[word].append(index)
-					valueSet.add(word)
-			else:
-				self.map[key].append(index)
-				valueSet.add(key)
+    def addGrams(self, wordSet):
+        for key in wordSet:
+            for word in key.split():
+                if word:
+                    self.grams['uni'][word[0]].append(key)
+                    if len(word) > 1:
+                        self.grams['bi'][word[:2]].append(key)
+                    if len(word) > 2:
+                        self.grams['tri'][word[:3]].append(key)
 
-		return valueSet
+    def getValues(self, value, splitWord=False):
+        valueSet = set()
 
-	def initGrams(self):
-		dataset = pd.read_csv('metadata.csv')
-		
-		titles = self.getValues(dataset['Title'].values.tolist())
-		albums = self.getValues(dataset['Album'].values.tolist())
-		artist = self.getValues(dataset['Artist'].values.tolist(), splitWord = True)
+        for index, v in enumerate(value):
+            v = v.lower()
+            key = re.sub(r'[^\w\s]', ' ', v)
+            if splitWord:
+                for word in key.split():
+                    self.map[word].append(index)
+                    valueSet.add(word)
+            else:
+                self.map[key].append(index)
+                valueSet.add(key)
 
-		self.metadata = {
-					'Title': titles,
-					'Artist': artist,
-					'Album': albums
-					}
+        return valueSet
 
-		self.addGrams(titles)
-		self.addGrams(artist)
-		self.addGrams(albums)
+    def initGrams(self):
+        dataset = pd.read_csv('metadata.csv')
 
+        titles = self.getValues(dataset['Title'].values.tolist())
+        albums = self.getValues(dataset['Album'].values.tolist())
+        artist = self.getValues(dataset['Artist'].values.tolist(), splitWord=True)
 
-	def searchType(self, searchWord):
-		r = re.compile('.*' + searchWord)
+        self.metadata = {
+            'Title': titles,
+            'Artist': artist,
+            'Album': albums
+        }
 
-		if len(searchWord) == 1:
-			result = self.grams['uni'].get(searchWord, [])
-		elif len(searchWord) == 2:
-			result = self.grams['bi'].get(searchWord, [])
-		elif len(searchWord) == 3:
-			result = self.grams['tri'].get(searchWord, [])
-		else:
-			result = list(filter(r.match, self.grams['tri'].get(searchWord[:3], [])))
+        self.addGrams(titles)
+        self.addGrams(artist)
+        self.addGrams(albums)
 
-		return result
+    def searchType(self, searchWord):
+        r = re.compile('.*' + searchWord)
 
-	def searchSong(self, searchString):
-		searchString = searchString.lower()
+        if len(searchWord) == 1:
+            result = self.grams['uni'].get(searchWord, [])
+        elif len(searchWord) == 2:
+            result = self.grams['bi'].get(searchWord, [])
+        elif len(searchWord) == 3:
+            result = self.grams['tri'].get(searchWord, [])
+        else:
+            result = list(filter(r.match, self.grams['tri'].get(searchWord[:3], [])))
 
-		result = self.searchType(searchString)
+        return result
 
-		if not result:
-			for words in searchString.split():
-				result += self.searchType(words)
-		
+    def searchSong(self, searchString):
+        searchString = searchString.lower()
 
-		finalResult = []
-		for r in result:
-			for k in self.metadata:
-				if r in self.metadata[k]:
-					finalResult.append([r, k])
-					break
+        result = self.searchType(searchString)
 
-		return finalResult
+        if not result:
+            for words in searchString.split():
+                result += self.searchType(words)
 
-	def addSong(self, data):
-		# data = (Artist, Title, Folder location)
-		
-		titles = self.getValues(data[1])	# Pass in title name
-		self.metadata['Title'] = self.metadata['Title'].union(titles)
-		self.addGrams(titles)
+        finalResult = []
+        for r in result:
+            for k in self.metadata:
+                if r in self.metadata[k]:
+                    finalResult.append([r, k])
+                    break
 
-		artist = self.getValues(data[0])	# Pass in artist name
-		self.metadata['Artist'] = self.metadata['Artist'].union(titles)
-		self.addGrams(artist)
+        return finalResult
 
-		songMetaData = {'Artist': data[0], 'Album': '', 'Title': data[1], 'Genre': '', 'Comments': '', 'music_folder': data[2]}	# Initialize song meta data with Artist, Album, Title, Genre, Comments, Music folder, Image Folder
+    def addSongToDB(self, data):
+        # data = (Artist, Title, Folder location)
 
-		# Getting cover image from the song file
-		mp3 = stagger.read_tag(data[2])		
-		if stagger.id3.APIC in mp3:
-			by_data = mp3[stagger.id3.APIC][0].data
-			im = io.BytesIO(by_data)
-			imageFile = Image.open(im)
-			imageFile.save(f'tmp/{title}-cover.jpg')
-			songMetaData['image_folder'] = f'tmp/{title}-cover.jpg'	# Add image location
+        titles = self.getValues(data[1])  # Pass in title name
+        self.metadata['Title'] = self.metadata['Title'].union(titles)
+        self.addGrams(titles)
 
-		# Adding it to metadata.csv
-		data = pd.read_csv('assets/data/metadata.csv')
-		data['Id'] = len(data)
-		data = data.append(data, ignore_index = True)
-		data.to_csv('assets/data/metadata.csv', index = False)
+        artist = self.getValues(data[0])  # Pass in artist name
+        self.metadata['Artist'] = self.metadata['Artist'].union(titles)
+        self.addGrams(artist)
 
-		# Getting the lyrics of the songs using genius library and adding it to index
-		# try:
-		# 	song = genius.search_song(title, artist)
-		# 	songMetaData.append(song.lyrics)
-		# except Exception as e:
-		#	songMetaData.append('')
-		# 	print(e)
+        # Initialize song meta data with Artist, Album, Title, Genre, Comments, Music folder, Image Folder
+        songMetaData = {'Artist': data[0], 'Album': '', 'Title': data[1], 'Genre': '', 'Comments': '', 'music_folder': data[2]}
 
-if __name__ == '__main__':	
-	print('Started')
-	s = Search()
-	print('Loaded')
+        # Getting cover image from the song file
+        mp3 = stagger.read_tag(data[2])
+        if stagger.id3.APIC in mp3:
+            by_data = mp3[stagger.id3.APIC][0].data
+            im = io.BytesIO(by_data)
+            imageFile = Image.open(im)
+            imageFile.save(f'tmp/{title}-cover.jpg')
+            songMetaData['image_folder'] = f'tmp/{title}-cover.jpg'  # Add image location
 
-	with open('map.json', 'w') as outfile:
-		json.dump(s.map, outfile)
+        # Adding it to metadata.csv
+        data = pd.read_csv('assets/data/metadata.csv')
+        data['Id'] = len(data)
+        data = data.append(data, ignore_index=True)
+        data.to_csv('assets/data/metadata.csv', index=False)
 
-	startTime = time.time()
-	print(s.searchSong('ed '))
-	print(time.time() - startTime)
+        # Getting the lyrics of the songs using genius library and adding it to index
+        # try:
+        # 	song = genius.search_song(title, artist)
+        # 	songMetaData.append(song.lyrics)
+        # except Exception as e:
+        #	songMetaData.append('')
+        # 	print(e)
 
 
+if __name__ == '__main__':
+    print('Started')
+    s = Search()
+    print('Loaded')
 
+    with open('map.json', 'w') as outfile:
+        json.dump(s.map, outfile)
 
-
-
+    startTime = time.time()
+    print(s.searchSong('ed '))
+    print(time.time() - startTime)
