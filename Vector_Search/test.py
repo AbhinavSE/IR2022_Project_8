6f4cbd4d28f1f234eb9d4c685fc7ad373c4520d5
frostrot <ankit18019@iiitd.ac.in>
2022-04-27 22:28:48 +0530

    added title parameter

--- Vector_Search/test.py
+++ Vector_Search/test.py
@@ -4,10 +4,15 @@ x = VectorSearch()
 print(x.query("you to hit the pedal", 2))
 print(x.query("shape of you", 5))
 print(len(x.data))
-print(x.add_song_indexing("I walk this empty street on the boulevard of broken dream."))
+print(
+    x.add_song_indexing(
+        "I walk this empty street on the boulevard of broken dream.",
+        "Boulevard of broken dreams",
+    )
+)
 print(len(x.data))
 print(x.query("you to hit the pedal", 2))
 print(x.query("shape of you", 5))
 print(x.query("empty street", 5))
-print(x.query("boulevard of broken dream", 5))
+print(x.query("boulevard of broken dream", 10))
 print(x.undo_addtion())
